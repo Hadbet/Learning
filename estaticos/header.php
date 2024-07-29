6f0ad6cc969c64f329f74cb0403f73090c1632b9
hadbet <hadbet.altamirano@grammer.com>
2024-07-29 13:15:57 -0600

    Pruebas con clases estaticas 02

--- estaticos/header.php
+++ estaticos/header.php
@@ -7,10 +7,6 @@ $claseEstatica = 'active';
 
 
 ?>
-
-<script>
-    alert('<?php echo $enlace_actual?>');
-</script>
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
     <div class="app-brand demo">
         <a href="../index.html" class="app-brand-link">
@@ -29,7 +25,7 @@ $claseEstatica = 'active';
 
     <ul class="menu-inner py-1">
         <!-- Dashboard -->
-        <li class="menu-item <?php if (str_contains($enlace_actual, 'index')) {echo $claseEstatica;}?>>">
+        <li class="menu-item <?php if (str_contains($enlace_actual, 'index')) {echo $claseEstaticaGlobal;}?>>">
             <a href="index.php" class="menu-link">
                 <i class="menu-icon tf-icons bx bx-home-circle"></i>
                 <div data-i18n="Analytics">Inicio</div>
